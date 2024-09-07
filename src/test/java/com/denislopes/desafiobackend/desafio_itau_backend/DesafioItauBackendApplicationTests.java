3464e6d811c29b65c25405ae6287f9efd2f375ad
DenisLopes <denislopes80@gmail.com>
2024-09-07 12:39:24 -0300

    Initial project setup

---
+++ src/test/java/com/denislopes/desafiobackend/desafio_itau_backend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denislopes.desafiobackend.desafio_itau_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
